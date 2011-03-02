0d99f27d8c533d00be3249e47bfb795bcef12668
kesalin <kesalin@gmail.com>
2011-03-02 06:06:45 +0000

    增加查找算法文件。

---
+++ SearchAlgorithms.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+#include <assert.h>
+
+#include "SearchAlgorithms.h"
+
+#define DEBUG_SORT
+
+#ifdef DEBUG_SORT
+#define debug_print(fmt,...) printf(fmt, __VA_ARGS__)
+#else
+#define debug_print(fmt,...)
+#endif
