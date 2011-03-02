9e40c2e47444e20b32b20336fc53fe2f5c334f55
kesalin@gmail.com <kesalin@gmail.com@a9e33781-49ad-ed23-8b7a-eb760121f030>
2011-03-02 09:40:48 +0000

    增加线性查找算法实现。

--- trunk/SearchAlgorithms.cpp
+++ trunk/SearchAlgorithms.cpp
@@ -12,3 +12,36 @@
 #else
 #define debug_print(fmt,...)
 #endif
+
+int sequential_search(int* array, int length, int key)
+{
+	assert(array && length >= 0);
+
+	int i;
+
+	for (i = 0; i < length; ++i) {
+		if (array[i] == value) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+// 在有序表 [0, length - 1]中进行二分查找。
+// 成功时返回结点的位置，失败时返回 -1
+int binary_search(int* array, int length, int key)
+{
+
+{ //在有序表R[1..n]中进行二分查找，成功时返回结点的位置，失败时返回零
+	int low=1，high=n，mid； //置当前查找区间上、下界的初值
+		while(low<=high){ //当前查找区间R[low..high]非空
+			mid=(low+high)/2；
+				if(R[mid].key==K) return mid； //查找成功返回
+					if(R[mid].kdy>K)
+						high=mid-1; //继续在R[low..mid-1]中查找
+					else
+						low=mid+1； //继续在R[mid+1..high]中查找
+		}
+		return 0； //当low>high时表示查找区间为空，查找失败
+} //BinSeareh
--- trunk/SearchAlgorithms.h
+++ trunk/SearchAlgorithms.h
@@ -1,4 +1,9 @@
 #ifndef __SEARCH_ALGORITHMS_H__
 #define __SEARCH_ALGORITHMS_H__
 
+// 线性查找:成功时返回结点的位置，失败时返回 -1
+int sequential_search(int* array, int length, int key);
+
+// 二分查找:成功时返回结点的位置，失败时返回 -1
+int binary_search(int* array, int length, int key);
 #endif // __SEARCH_ALGORITHMS_H__
