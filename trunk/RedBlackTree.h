8b0107c264d99d045615af57914deac2b2819dba
kesalin@gmail.com <kesalin@gmail.com@a9e33781-49ad-ed23-8b7a-eb760121f030>
2011-03-13 14:43:42 +0000

    添加红黑树插入节点的实现。

--- trunk/RedBlackTree.h
+++ trunk/RedBlackTree.h
@@ -1,5 +1,32 @@
 #ifndef __RED_BLACK_TREE_H__
 #define __RED_BLACK_TREE_H__
 
+enum RBColor{
+	RB_Red,
+	RB_Black,
+};
+
+struct RBNode 
+{
+	RBColor color;
+	int key;
+	RBNode* leftChild;
+	RBNode* rightChild;
+	RBNode* parent;
+};
+
+typedef RBNode* RBTree;
+
+static RBNode RBNode_Nil = {RB_Black, 0, 0, 0, 0};
+
+// 最小关键字元素
+RBNode* RBTree_minimum(RBNode* x);
+
+// 前驱
+RBNode* RBTree_successor(RBNode* x);
+
+void RBTree_insert(RBTree* tree, RBNode* node);
+
+RBNode* RBTree_delete(RBTree* tree, RBNode* node);
 
 #endif	// __RED_BLACK_TREE_H__
